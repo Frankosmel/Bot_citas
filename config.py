693b7ddc6dd11e72dfc8f6e682cc7d2672952c8b
Frankosmel <130179322+Frankosmel@users.noreply.github.com>
2025-07-21 09:15:45 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
-TELEGRAM_TOKEN = "7733430687:AAEQnxvcIi4GQEM2NhJrmhvDc_zAdxdPXnI"
+TELEGRAM_TOKEN = "8173125440:AAEg07t98S5BEswJR3KzOrXXz6nFDgiFeNk"
 DB_URL         = "sqlite:///botcitas.db"  # o la URL que uses (Postgres, MySQL…)
 ADMIN_ID       = 1383931339               # tu ID de Telegram como entero
